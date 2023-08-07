251dfb6170e86cf18fcd06a25fb4f530aadd82f5
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-07 21:46:50 +0200

    delete operator for

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,5 @@ public class Main {
 
         System.out.println("Hello and welcome!");
 
-
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
         }
     }
-}
